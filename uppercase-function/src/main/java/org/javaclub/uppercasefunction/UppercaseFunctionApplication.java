a906e7fae8f295f4219c472446b50db0cf44c7af
alevchenko <alevchenko@lohika.com>
2019-01-24 10:20:21 +0200

    added second function module

---
+++ uppercase-function/src/main/java/org/javaclub/uppercasefunction/UppercaseFunctionApplication.java
@@ -0,0 +1,23 @@
+package org.javaclub.uppercasefunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class UppercaseFunctionApplication {
+
+    @Bean
+    public Function<Flux<String>, Flux<String>> uppercase() {
+        return flux -> flux.map(value -> value.toUpperCase());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(UppercaseFunctionApplication.class, args);
+    }
+
+}
+
